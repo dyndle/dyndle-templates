7064724d329d8301166f99c244dff39f3ac978e1
quirijnslings <quirijn.slings@trivident.com>
2020-04-02 10:56:31 +0200

    added sonar-scanner

--- build-tools/sonar-scanner.bat
+++ build-tools/sonar-scanner.bat
@@ -1,3 +1,3 @@
-%SONAR_SCANNER% begin /k:"trivident_dyndle-templates" /o:trivident-bitbucket /d:sonar.verbose=true /d:sonar.login=%SONAR_LOGIN% /d:sonar.host.url=https://sonarcloud.io
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" ..\src\Dyndle.Templates.sln" /t:Rebuild 
-%SONAR_SCANNER% end /d:sonar.login=%SONAR_LOGIN% 
+"%SONAR_SCANNER%" begin /k:"trivident_dyndle-templates" /o:trivident-bitbucket /d:sonar.verbose=true /d:sonar.login=%SONAR_LOGIN% /d:sonar.host.url=https://sonarcloud.io
+"%MSBUILD%" ..\src\Dyndle.Templates.sln /t:Rebuild 
+"%SONAR_SCANNER%" end /d:sonar.login=%SONAR_LOGIN% 
